d7fd409c43fa035df52ed43cbf6088ad8465afb1
eric-fz <123209332+eric-fz@users.noreply.github.com>
2023-03-31 02:06:04 -0400

    almost fixed

--- gear.py
+++ gear.py
@@ -2,7 +2,7 @@
 import numpy as np
 import scipy.sparse as sp
 # Set the number of nodes in the graph
-num_nodes = 10
+num_nodes = 3
 # Set edge density
 edge_density = 0.1
 
@@ -12,8 +12,8 @@ num_node_features = 3
 
 connected = False
 while not connected:
-    # Instantiate an empty connectivity matrix with the desired shape
-    connectivity_matrix = sp.rand(num_nodes, num_nodes, density=edge_density)
+    # Instantiate a connectivity matrix with the desired shape
+    connectivity_matrix = sp.random(num_nodes, num_nodes, density=edge_density)
     connectivity_matrix = np.array(connectivity_matrix.todense())
     #Fill diagonals with 0s
     np.fill_diagonal(connectivity_matrix,0)
@@ -21,25 +21,29 @@ while not connected:
     connectivity_matrix = np.triu(connectivity_matrix) + np.triu(connectivity_matrix, 1).T
 
     # ensure connectivity
-    _, components = sp.csgraph.connected_components()
+    _, components = sp.csgraph.connected_components(connectivity_matrix, directed=False)
     if components[0] == components[-1]:
         connected = True
+
+connectivity_matrix[connectivity_matrix != 0] = 1
+print(connectivity_matrix)
 
 # Loop through the edges in the graph and assign their features
 for i in range(num_nodes):
     for j in range(num_nodes):
-        if i != j & connectivity_matrix[i,j,0] == 1:
+        if (i != j) & (connectivity_matrix[i,j] == 1):
             # Define the edge features as desired
-            m = #module
-            t1 = #teeth 1
-            t2 = #teeth 2
-            b = #gear thickness
-            alpha = #working contact angle
-            x1 = #profile shift 1
-            x2 = #profile shift 2
+            m = 3 #module
+            t1 = 4 #teeth 1
+            t2 = 5 #teeth 2
+            b = 6 #gear thickness
+            alpha = 7 #working contact angle
+            x1 = 8 #profile shift 1
+            x2 = 9 #profile shift 2
             # Increase dimension of connectivity matrix
             # Assign the edge features to the connectivity matrix
-            np.expand_dims(connectivity_matrix, axis = 0)
+            connectivity_matrix = np.expand_dims(connectivity_matrix, axis=0)
+            print(connectivity_matrix)
             connectivity_matrix[i, j, 1] = m
             connectivity_matrix[i, j, 2] = t1
             connectivity_matrix[i, j, 3] = t2
@@ -47,7 +51,6 @@ for i in range(num_nodes):
             connectivity_matrix[i, j, 5] = alpha
             connectivity_matrix[i, j, 6] = x1
             connectivity_matrix[i, j, 7] = x2
-
 
 # Instantiate an empty node feature matrix with the desired shape
 node_feature_matrix = np.zeros((num_nodes, num_node_features))
